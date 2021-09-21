5c3e3b2f06671b906957b4cf3054a1517277df44
BigBertman <brysonbertuzzi@hotmail.com>
2021-09-21 19:31:14 -0400

    Redid the Camera rotation (wasn't working with arm)

--- Prototype Horror Game/Assets/Scripts/PlayerController.cs
+++ Prototype Horror Game/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,11 @@ public class PlayerController : MonoBehaviour
     private float m_JumpHeight = 1.0f; // Jump Height
     private float gravityValue = -9.81f; // Gravity
 
+    public GameObject m_Hand;
+
+    Vector3 m_HandLocalPosition;
+    public Transform DefaultHandPosition;
+
     // Camera
     public Camera m_PlayerCamera; // First Person Camera
     public float m_RotationSpeed = 2.0f;// Rptation Speed of Camera
@@ -59,29 +64,69 @@ public class PlayerController : MonoBehaviour
         MoveCharacter();
 
         // Character Rotation
-        CameraRotation();
+        // CameraRotation();
 
         // Character Jump and Gravity
-        JumpAndGravity();
+        //JumpAndGravity();
+
+        UpdateHand();
+
+        transform.Rotate(
+          new Vector3(0f, (m_Controller.GetLookInput().x * m_RotationSpeed * 1.0f),
+              0f), Space.Self);
+
+        {
+            // add vertical inputs to the camera's vertical angle
+            m_CameraVerticalAngle += m_Controller.GetLookInput().y * m_RotationSpeed * 1.0f;
+
+            // limit the camera's vertical angle to min/max
+            m_CameraVerticalAngle = Mathf.Clamp(m_CameraVerticalAngle, -89f, 89f);
+
+            // apply the vertical angle as a local rotation to the camera transform along its right axis (makes it pivot up and down)
+            m_PlayerCamera.transform.localEulerAngles = new Vector3(m_CameraVerticalAngle, 0, 0);
+        }
+
     }
 
     public void CameraRotation()
     {
-        // Set PlayerRotation to mouse input
-        m_PlayerRotation += m_Controller.GetLookInput() * m_RotationSpeed;
-        m_PlayerRotation.z = 0.0f;
+        //{
+        //    // Set PlayerRotation to mouse input
+        //    m_PlayerRotation += m_Controller.GetLookInput() * m_RotationSpeed;
+        //    m_PlayerRotation.z = 0.0f;
 
-        // Clamp Player Rotation to (-90, 90)
-        m_PlayerRotation.y = Mathf.Clamp(m_PlayerRotation.y, -90.0f, 90.0f);
+        //    // Clamp Player Rotation to (-90, 90)
+        //    m_PlayerRotation.y = Mathf.Clamp(m_PlayerRotation.y, -90.0f, 90.0f);
 
-        // Get a rotation based on mouse input around (0, 1, 0)
-        Quaternion xQuat = Quaternion.AngleAxis(m_PlayerRotation.x, Vector3.up);
+        //    // Get a rotation based on mouse input around (0, 1, 0)
+        //    Quaternion xQuat = Quaternion.AngleAxis(m_PlayerRotation.x, Vector3.up);
 
-        // Get a rotation based on mouse input around (-1, 0, 0)
-        Quaternion yQuat = Quaternion.AngleAxis(m_PlayerRotation.y, Vector3.left);
+        //    // Get a rotation based on mouse input around (-1, 0, 0)
+        //    Quaternion yQuat = Quaternion.AngleAxis(m_PlayerRotation.y, Vector3.left);
 
-        // Change the players camera rotation based on Quaternion
-        m_PlayerCamera.transform.localRotation = xQuat * yQuat;
+        //    // Change the players camera rotation based on Quaternion
+        //    m_PlayerCamera.transform.rotation = xQuat * yQuat;
+        //}
+
+        // Horizontal character rotation
+        {
+            // Rotate the transform with the input speed around its local Y axis
+            transform.Rotate(
+                new Vector3(0f, (m_Controller.GetLookInput().x * m_RotationSpeed * 1.0f),
+                    0f), Space.Self);
+        }
+
+        // Vertical camera rotation
+        {
+            // Add vertical inputs to the camera's vertical angle
+            m_CameraVerticalAngle += m_Controller.GetLookInput().z * m_RotationSpeed * 1.0f;
+
+            // Limit the camera's vertical angle to min/max
+            m_CameraVerticalAngle = Mathf.Clamp(m_CameraVerticalAngle, -89f, 89f);
+
+            // Apply the vertical angle as a local rotation to the camera transform along its right axis (makes it pivot up and down)
+            m_PlayerCamera.transform.localEulerAngles = new Vector3(m_CameraVerticalAngle, 0, 0);
+        }
     }
 
     public void MoveCharacter()
@@ -96,6 +141,9 @@ public class PlayerController : MonoBehaviour
 
         // Change the players movement based on the targetDirection
         m_CharacterController.Move(targetDirection * m_Speed * Time.deltaTime);
+
+
+        //  m_Hand.transform.position = targetDirection + m_Hand.transform.position;
     }
 
 
@@ -113,5 +161,22 @@ public class PlayerController : MonoBehaviour
 
         // Change the players movement based on the Jump Velocity
         m_CharacterController.Move(m_JumpVelocity * Time.deltaTime);
+    }
+
+    public void UpdateHand()
+    {
+        //m_Hand.transform.rotation = m_PlayerCamera;
+        Vector3 diff = m_Controller.GetLookInput();
+        diff.Normalize();
+
+        float rotation = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+
+        // m_Hand.transform.rotation = m_PlayerCamera.transform.rotation;
+        //m_Hand.transform.LookAt(transform);
+        //m_Hand.transform.Rotate(m_Controller.GetLookInput());
+
+        m_HandLocalPosition = Vector3.Lerp(m_HandLocalPosition, DefaultHandPosition.localPosition, m_Speed * Time.deltaTime);
+
+
     }
 }
