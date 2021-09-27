7707df00cab816e059a86275367a9a0023c6edc2
BigBertman <brysonbertuzzi@hotmail.com>
2021-09-27 14:47:20 -0400

    Cleaned up Door.cs

--- Prototype Horror Game/Assets/Scripts/Door.cs
+++ Prototype Horror Game/Assets/Scripts/Door.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    Animator m_Door;
-    bool m_DoorAnimLock = true;
+    Animator m_Door;                // Door Animator
+    bool m_DoorAnimLock = true;     // Is Door Locked?
 
     // Start is called before the first frame update
     void Start()
     {
+        // Fetch components on the same gameObject
         m_Door = GetComponent<Animator>();
     }
 
@@ -19,11 +20,16 @@ public class Door : MonoBehaviour
 
     }
 
+    // OpenDoor is called when opening door
     public void OpenDoor()
     {
+        // If Door is Locked...
         if (m_DoorAnimLock)
         {
+            // Unlock door
             m_DoorAnimLock = false;
+
+            // Update Door Animator m_IsOpen
             m_Door.SetBool("m_IsOpen", true);
         }
     }
